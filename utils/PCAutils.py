60313e35c573428c60a40a9ae7aeb008436f13ba
Linghao Kong <linghaokong2000@gmail.com>
2024-06-17 12:23:11 -0400

    Adds clustering and PCA utils

---
+++ utils/PCAutils.py
@@ -0,0 +1,24 @@
+import time
+from cuml import PCA
+
+def make_and_apply_PCA(inputs, hidden_dim, reduction_factor, verbose = False):
+    if verbose:
+        print("Making and applying PCA with reduction factor: ", reduction_factor)
+        tick = time.time()
+    pca_model = PCA(
+        n_components=hidden_dim//reduction_factor,
+        random_state=0,
+        svd_solver="auto",
+    )
+    transformed_inputs = pca_model.fit_transform(
+        inputs.reshape(
+            -1, inputs.shape[-1]
+        )
+    )
+    if verbose:
+        tock = time.time()
+        print("Time taken: ", tock - tick)
+        print("Top 10 explained variance ratios: ", pca_model.explained_variance_ratio_[:10])
+        print("Bottom 10 explained variance ratios: ", pca_model.explained_variance_ratio_[-10:])
+        print("Overall explained variance ratio: ", pca_model.explained_variance_ratio_.sum())
+    return pca_model, transformed_inputs
---
+++ utils/clusteringutils.py
@@ -0,0 +1,31 @@
+import time
+from cuml import KMeans
+import numpy as np
+import cupy as cp
+
+def make_and_apply_KMeans(inputs, n_clusters, verbose = False):
+    if verbose:
+        print("Making and applying KMeans with n_clusters: ", n_clusters)
+        tick = time.time()
+    kmeans_model = KMeans(
+        n_clusters=n_clusters,
+        random_state=0,
+        n_init=100,
+    )
+    kmeans_model.fit(
+        inputs.reshape(
+            -1, inputs.shape[-1]
+        )
+    )
+    if verbose:
+        tock = time.time()
+        print("Time taken: ", tock - tick)
+        unique, counts = np.unique(
+            cp.asnumpy(kmeans_model.labels_), 
+            return_counts=True,
+        )
+        total = counts.sum()
+        print(f"Proportion of each cluster:")
+        for i in range(len(unique)):
+            print(f"{unique[i]}: {counts[i]/total}")
+    return kmeans_model
